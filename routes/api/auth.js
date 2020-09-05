c6dc8b2c653c3ba984df058641d832025fe2845d
btran8337 <btran8337@yahoo.com>
2020-09-05 18:36:39 -0500

    ready to deploy

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -8,7 +8,7 @@ const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
 // @route   GET api/auth
-// @desc    Test route
+// @desc    Register user
 // @access  Public
 router.get('/', auth, async (req, res) => {
   try {
